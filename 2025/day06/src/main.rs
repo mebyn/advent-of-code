2e5f9688be46b86a1c12173b1609b6d853486378
Melvin <melvin.sy@importgenius.com>
2025-12-06 15:58:03 +0800

    ✨ feat: Day 06 AoC 2025 solution 01

--- 2025/day06/src/main.rs
+++ 2025/day06/src/main.rs
@@ -10,9 +10,37 @@ fn main() {
     println!("Result: {}", result);
 }
 
-fn solution(input: &str) -> i64 {
-    // TODO: Implement solution
-    0
+fn solution(input: &str) -> u64 {
+    let lines = input.trim().lines().collect::<Vec<&str>>();
+    let grid: Vec<Vec<&str>> = lines
+        .iter()
+        .map(|line| line.trim().split_whitespace().collect())
+        .collect();
+
+    let mut numbers: Vec<Vec<&str>> = Vec::new();
+    for x in 0..grid[0].len() {
+        let mut row = Vec::new();
+        for y in 0..grid.len() {
+            row.push(grid[y][x]);
+        }
+        numbers.push(row);
+    }
+    println!("Numbers = {:?}", numbers);
+    numbers
+        .iter()
+        .map(|problem| {
+            let operation = problem.last().unwrap();
+            let numbers = problem[..problem.len() - 1]
+                .iter()
+                .map(|s| s.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
+            match *operation {
+                "*" => numbers.iter().product(),
+                "+" => numbers.iter().sum(),
+                _ => 0,
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -21,7 +49,12 @@ mod tests {
 
     #[test]
     fn test_sample_input() {
-        let input = ""; // Add test input
-        assert_eq!(solution(input), 0);
+        let input = "
+        123 328  51 64 
+        45 64  387 23 
+        6 98  215 314
+        *   +   *   + 
+        ";
+        assert_eq!(solution(input), 4277556);
     }
 }
